5bc81af8acdf3f179097276bcfbec00e0392f882
Pedro Filho <pedro@filho.me>
2019-08-09 22:48:47 -0300

    adding meta title and description

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -9,6 +9,13 @@ const SEO: FC<ISEO> = ({ title = "Software Engineer" }) => (
   <Head>
     <title>Pedro Filho - {title}</title>
 
+    <meta name="title" content="Pedro Filho - Software Engineer" />
+
+    <meta
+      name="description"
+      content="Curious by nature, entrepreneur, developer practitioner of agile philosophy and JavaScript enthusiast."
+    />
+
     <link rel="icon" href="../static/favicon.ico" />
   </Head>
 );
