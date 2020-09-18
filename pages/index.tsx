90f6665babe1d06596789f530026012ffa6c01a5
Pedro Filho <pedro@filho.me>
2020-09-18 21:00:05 +0100

    Fixing accessibility issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,7 @@ const Index: FC = () => (
       />
       <meta
         name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
       />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:url" content="https://pedroapfilho.com" />
