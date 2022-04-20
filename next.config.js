47e2732670ce8f8a42b8bbe24a84392e55b96ff4
Pedro Filho <pedro@filho.me>
2022-04-20 12:38:38 +0200

    removing babel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
 };
+
+module.exports = nextConfig;
