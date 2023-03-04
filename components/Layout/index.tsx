2b64152a16dc5a7156c922bd4456132c84e2ca2f
Pedro Filho <pedro@filho.me>
2023-03-04 01:11:24 +0000

    Update index.tsx

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import styled from "styled-components";
 
 const Container = styled.main`
-  height: 100vh;
+  height: 100dvh;
 
   box-sizing: border-box;
   padding: 2em;
