e4137e97c680a5e26fa3a9ddd5c2d6bec9152f48
Pedro Filho <pedro@filho.me>
2026-01-12 15:38:26 -0300

    feat: updating resume

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    optimizeCss: true,
-  },
 };
 
 export default nextConfig;
