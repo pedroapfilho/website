272fe781b388fe7e3d4138e209a445fa5d5bc08f
Pedro Filho <pedro@filho.me>
2026-09-03 18:04:31 -0300

    chore: replace clsx and tailwind-merge with shadcn's cn package
    lib/utils.ts becomes a re-export of cn so the @/lib/utils import shadcn
    components expect keeps working.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,1 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-const cn = (...inputs: Array<ClassValue>) => twMerge(clsx(inputs));
-
-export { cn };
+export { cn } from "cn";
